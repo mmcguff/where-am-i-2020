cced1f54c8313e8b2d2db643a125e391c68bdced
Matthew McGuff <mmcguff@byui.edu>
2020-06-13 08:15:19 -0600

    Add current stage of map development.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapsService } from './maps.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'where-am-i';
+
+  lat: string = '';
+  lng: string = '';
+
+  location: Object;
+
+  constructor(private map: MapsService) {}
+
+  ngOnInit() {
+
+
+    //Best location is from the primary location service
+
+      //secondary is from ip address that will get it close enough
+
+      this.map.getLocation().subscribe(data => {
+        this.lat = data.latitude;
+        this.lng = data.longitude;
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.lat = position.coords.latitude.toString()
+          this.lng = position.coords.longitude.toString()
+        });
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyChCNU2B-GSA5dgfJJJ36RpMjXSLLKjTXk'
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
